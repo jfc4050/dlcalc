a189621f8c241c08f83ebb4ba87c90d46227590f
Justin Chiu <jfc4050@gmail.com>
2024-05-08 22:34:40 -0700

    comm equation refactoring

--- dlcalc/utils/comms.py
+++ dlcalc/utils/comms.py
@@ -7,13 +7,10 @@ from .data import Size
 from .hardware import MachineSpec
 
 
-def _ring_ag_or_rs_comm_time_s(size: Size, n_participants: int, machine_spec: MachineSpec) -> int:
-    n = size
-    p = n_participants
+def _ring_ag_or_rs_comm_time_s(size: Size, n_participants: int, machine_spec: MachineSpec) -> float:
+    phase_sent_data_size_bits = int(size.bits() / n_participants)
 
-    phase_sent_data_size_bits = int(n.bits() / p)
-
-    n_phases = p - 1
+    n_phases = n_participants - 1
 
     # divide full BW among devices (which will be part of different DP groups)
     # TODO. this is a safe assumption vast majority of the time but might
@@ -29,14 +26,14 @@ def _ring_ag_or_rs_comm_time_s(size: Size, n_participants: int, machine_spec: Ma
 
 def get_reduce_scatter_comm_time_s(
     size: Size, n_participants: int, machine_spec: MachineSpec
-) -> int:
+) -> float:
     """assumes ring algorithm."""
     return _ring_ag_or_rs_comm_time_s(
         size, n_participants=n_participants, machine_spec=machine_spec
     )
 
 
-def get_all_gather_comm_time_s(size: Size, n_participants: int, machine_spec: MachineSpec) -> int:
+def get_all_gather_comm_time_s(size: Size, n_participants: int, machine_spec: MachineSpec) -> float:
     """assumes ring algorithm."""
     return _ring_ag_or_rs_comm_time_s(
         size, n_participants=n_participants, machine_spec=machine_spec
