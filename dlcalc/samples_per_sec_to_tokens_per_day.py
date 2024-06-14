9b7fb64a2326b33c812578fbba286f78983b700b
Justin Chiu <jfc4050@gmail.com>
2024-06-14 13:26:32 -0700

    added goodput accounting for sps2tpd

--- dlcalc/samples_per_sec_to_tokens_per_day.py
+++ dlcalc/samples_per_sec_to_tokens_per_day.py
@@ -19,16 +19,29 @@ def main() -> None:
         required=True,
         help="sequence length in tokens (i.e. tokens/sample)",
     )
+    parser.add_argument(
+        "-g",
+        "--goodput",
+        type=float,
+        default=1.0,
+        required=False,
+        help="training goodput. expected to be in range [0.0, 1.0]",
+    )
     args = parser.parse_args()
 
     samples_per_sec = args.samples_per_sec
     tokens_per_sample = args.seqlen
+    goodput = args.goodput
+
+    if not 0.0 <= goodput <= 1.0:
+        raise ValueError(f"goodput expected to be in range [0.0, 1.0] but got {goodput}")
 
     tokens_per_day = samples_per_sec * tokens_per_sample * 60 * 60 * 24
 
     print(
         f"{samples_per_sec} samples/sec "
         f"with sequence length {tokens_per_sample} "
+        f"and {goodput * 100:.2f}% goodput "
         f"translates to {tokens_per_day * 1e-9:.2f}B tokens/day"
     )
 
